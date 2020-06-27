15244055d49291492bc765e48b0d153c4a48cf1f
Thiago Brasil <thiago.brasil@sitiobarreiras.com.br>
2020-06-27 17:30:26 -0300

    add tests about welcome message visibility

--- tests/unit/form.spec.js
+++ tests/unit/form.spec.js
@@ -20,4 +20,80 @@ describe('Form', () => {
 
     expect(wrapper.find('h2').text()).toBe('Welcome thiago brasil');
   });
+
+  describe('computed', () => {
+    describe('fullname', () => {
+      it('should be equals to {name lastname}', () => {
+        const wrapper = shallowMount(Form, {
+          data: () => ({
+            name: 'thiago',
+            lastName: 'brasil',
+          }),
+        });
+        expect(wrapper.vm.fullname).toBe('thiago brasil');
+      });
+      describe('isFormCompleted', () => {
+        it('should be true when has name and lastName', () => {
+          const wrapper = shallowMount(Form, {
+            data: () => ({
+              name: 'thiago',
+              lastName: 'brasil',
+            }),
+          });
+          expect(wrapper.vm.isFormCompleted).toBe(true);
+        });
+        it('should be false when has only name', () => {
+          const wrapper = shallowMount(Form, {
+            data: () => ({
+              name: 'thiago',
+            }),
+          });
+          expect(wrapper.vm.isFormCompleted).toBe(false);
+        });
+        it('should be false when has only lastName', () => {
+          const wrapper = shallowMount(Form, {
+            data: () => ({
+              lastName: 'brasil',
+            }),
+          });
+          expect(wrapper.vm.isFormCompleted).toBe(false);
+        });
+        it('should be false when has (name and lastName) empty', () => {
+          const wrapper = shallowMount(Form, {
+            data: () => ({
+              name: '',
+              lastName: '',
+            }),
+          });
+          expect(wrapper.vm.isFormCompleted).toBe(false);
+        });
+      });
+    });
+  });
+
+  describe('Welcome message', () => {
+    describe('When is form completed', () => {
+      it('should show welcome message', () => {
+        const wrapper = shallowMount(Form, {
+          data: () => ({
+            name: 'thiago',
+            lastName: 'brasil',
+          }),
+        });
+        expect(wrapper.find('.form_h2_welcome_message').text()).toMatch('Welcome thiago brasil');
+        expect(wrapper.find('.form_h2_welcome_message').exists()).toBe(true);
+      });
+    });
+    describe('When is form completed', () => {
+      it('should not show welcome message', () => {
+        const wrapper = shallowMount(Form, {
+          data: () => ({
+            name: 'thiago',
+            // lastName: 'brasil',
+          }),
+        });
+        expect(wrapper.find('.form_h2_welcome_message').exists()).toBe(false);
+      });
+    });
+  });
 });
