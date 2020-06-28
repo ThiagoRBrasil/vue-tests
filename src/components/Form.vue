ec3d5eb5acda9b031492b57fec66d2394fd8cdfe
Thiago Brasil <thiago.brasil@sitiobarreiras.com.br>
2020-06-28 09:24:09 -0300

    test validate string

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -10,6 +10,7 @@
       <label>Last Name:</label>
       <input v-model="lastName" class="form_label_lastname" type="text" name="formLastName" />
     </div>
+    <h1 v-if="isFormCompleted" class="form_h1_address">Address</h1>
   </div>
 </template>
 
@@ -24,7 +25,28 @@ export default {
       return `${this.name} ${this.lastName}`;
     },
     isFormCompleted() {
-      return !!(this.name && this.lastName);
+      const { isValidString, hasOnlyLetters, name, lastName } = this;
+      return (
+        isValidString(name) &&
+        isValidString(lastName) &&
+        hasOnlyLetters(name) &&
+        hasOnlyLetters(lastName)
+      );
+    },
+    isValidName() {
+      return this.name.length > 1;
+    },
+    isValidLastName() {
+      return this.name.lastName > 1;
+    },
+  },
+  methods: {
+    isValidString(string) {
+      return string.length > 1;
+    },
+    hasOnlyLetters(string) {
+      const lettersRegex = /^[A-Za-z]+$/;
+      return !!string.match(lettersRegex);
     },
   },
 };
--- tests/unit/form.spec.js
+++ tests/unit/form.spec.js
@@ -23,50 +23,71 @@ describe('Form', () => {
 
   describe('computed', () => {
     describe('fullname', () => {
-      it('should be equals to {name lastname}', () => {
+      it("should be equals to 'name lastname'", () => {
         const wrapper = shallowMount(Form, {
-          data: () => ({
-            name: 'thiago',
-            lastName: 'brasil',
-          }),
+          data: () => ({ name: 'edimo', lastName: 'sousa' }),
         });
-        expect(wrapper.vm.fullname).toBe('thiago brasil');
+        expect(wrapper.vm.fullname).toBe('edimo sousa');
       });
-      describe('isFormCompleted', () => {
-        it('should be true when has name and lastName', () => {
-          const wrapper = shallowMount(Form, {
-            data: () => ({
-              name: 'thiago',
-              lastName: 'brasil',
-            }),
+    });
+    describe('isFormCompleted', () => {
+      describe('When has name and lastName', () => {
+        describe('And name and lastName are valids', () => {
+          it('should be true', () => {
+            const wrapper = shallowMount(Form, {
+              data: () => ({ name: 'edimo', lastName: 'sousa' }),
+            });
+            expect(wrapper.vm.isFormCompleted).toBe(true);
           });
-          expect(wrapper.vm.isFormCompleted).toBe(true);
         });
-        it('should be false when has only name', () => {
-          const wrapper = shallowMount(Form, {
-            data: () => ({
-              name: 'thiago',
-            }),
+
+        describe('And name and lastName are NOT valids', () => {
+          it('should be false When name length is lesser than 2', () => {
+            const wrapper = shallowMount(Form, {
+              data: () => ({ name: 'e', lastName: 'sousa' }),
+            });
+            expect(wrapper.vm.isFormCompleted).toBe(false);
           });
-          expect(wrapper.vm.isFormCompleted).toBe(false);
-        });
-        it('should be false when has only lastName', () => {
-          const wrapper = shallowMount(Form, {
-            data: () => ({
-              lastName: 'brasil',
-            }),
+          it('should be false When lastName length is lesser than 2', () => {
+            const wrapper = shallowMount(Form, {
+              data: () => ({ name: 'edimo', lastName: 's' }),
+            });
+            expect(wrapper.vm.isFormCompleted).toBe(false);
           });
-          expect(wrapper.vm.isFormCompleted).toBe(false);
-        });
-        it('should be false when has (name and lastName) empty', () => {
-          const wrapper = shallowMount(Form, {
-            data: () => ({
-              name: '',
-              lastName: '',
-            }),
+          it('should be false When name contains number', () => {
+            const wrapper = shallowMount(Form, {
+              data: () => ({ name: 'e123', lastName: 'sousa' }),
+            });
+            expect(wrapper.vm.isFormCompleted).toBe(false);
           });
-          expect(wrapper.vm.isFormCompleted).toBe(false);
+          it('should be false When lastname contains number', () => {
+            const wrapper = shallowMount(Form, {
+              data: () => ({ name: 'edimo', lastName: '123' }),
+            });
+            expect(wrapper.vm.isFormCompleted).toBe(false);
+          });
         });
+      });
+
+      it('should be false when has only name', () => {
+        const wrapper = shallowMount(Form, {
+          data: () => ({ name: 'edimo', lastName: '' }),
+        });
+        expect(wrapper.vm.isFormCompleted).toBe(false);
+      });
+
+      it('should be false when has only lastName', () => {
+        const wrapper = shallowMount(Form, {
+          data: () => ({ name: '', lastName: 'sousa' }),
+        });
+        expect(wrapper.vm.isFormCompleted).toBe(false);
+      });
+
+      it('should be false when has (name and lastName) empty', () => {
+        const wrapper = shallowMount(Form, {
+          data: () => ({ name: '', lastName: '' }),
+        });
+        expect(wrapper.vm.isFormCompleted).toBe(false);
       });
     });
   });
@@ -75,25 +96,87 @@ describe('Form', () => {
     describe('When is form completed', () => {
       it('should show welcome message', () => {
         const wrapper = shallowMount(Form, {
-          data: () => ({
-            name: 'thiago',
-            lastName: 'brasil',
-          }),
+          data: () => ({ name: 'thiago', lastName: 'brasil' }),
         });
-        expect(wrapper.find('.form_h2_welcome_message').text()).toMatch('Welcome thiago brasil');
+
+        expect(wrapper.find('.form_h2_welcome_message').text()).toBe('Welcome thiago brasil');
         expect(wrapper.find('.form_h2_welcome_message').exists()).toBe(true);
       });
     });
-    describe('When is form completed', () => {
-      it('should not show welcome message', () => {
+
+    describe('When form is not completed', () => {
+      it('not show welcome message', async () => {
         const wrapper = shallowMount(Form, {
-          data: () => ({
-            name: 'thiago',
-            // lastName: 'brasil',
-          }),
+          data: () => ({ name: 'edimo', lastName: '' }),
         });
+
         expect(wrapper.find('.form_h2_welcome_message').exists()).toBe(false);
       });
+    });
+  });
+  describe('Address', () => {
+    describe('When name and lastname are filled', () => {
+      const wrapper = shallowMount(Form, {
+        computed: { isFormCompleted: () => true },
+      });
+      it('should has Address label', () => {
+        expect(wrapper.find('.form_h1_address').text()).toBe('Address');
+        expect(wrapper.find('.form_h1_address').exists()).toBe(true);
+      });
+    });
+
+    describe('When name and lastname are NOT filled', () => {
+      const wrapper = shallowMount(Form, {
+        computed: { isFormCompleted: () => false },
+      });
+      it('should NOT has Address label', () => {
+        expect(wrapper.find('.form_h1_address').exists()).toBe(false);
+      });
+    });
+
+    describe('Validations', () => {
+      describe('hasOnlyLetters', () => {
+        describe('When has only letters', () => {
+          it('return true', () => {
+            const wrapper = shallowMount(Form);
+
+            expect(wrapper.vm.hasOnlyLetters('thiago')).toBe(true);
+          });
+          it('return false', () => {
+            const wrapper = shallowMount(Form);
+
+            expect(wrapper.vm.hasOnlyLetters('123')).toBe(false);
+          });
+        });
+      });
+      describe('isValidString', () => {
+        describe('When string is valid', () => {
+          it('return true', () => {
+            const wrapper = shallowMount(Form);
+
+            expect(wrapper.vm.isValidString('thiago')).toBe(true);
+          });
+        });
+        describe('When string is NOT valid', () => {
+          it('return false', () => {
+            const wrapper = shallowMount(Form);
+
+            expect(wrapper.vm.isValidString('t')).toBe(false);
+          });
+        });
+      });
+    });
+  });
+  describe('!!', () => {
+    it('return true when receive a string', () => {
+      expect(!!'thigo').toBe(true);
+
+      expect(!!false).toBe(false);
+      expect(!!NaN).toBe(false);
+      expect(!!0).toBe(false);
+      expect(!!null).toBe(false);
+      expect(!!undefined).toBe(false);
+      expect(!!'').toBe(false);
     });
   });
 });
